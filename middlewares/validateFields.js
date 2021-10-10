8c74b75d5c53dec3c08bf31294fd1d20d6f8276f
arturfil <arturfil@hotmail.com>
2021-10-10 18:22:53 -0400

    added validation on the backend

---
+++ middlewares/validateFields.js
@@ -0,0 +1,13 @@
+const { valiationResult, validationResult } = require('express-validator');
+
+const validateFields = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors);
+  }
+  next();
+}
+
+module.exports = {
+  validateFields
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { check } = require('express-validator');
+
 const {
   getAllUsers,
   loginUser,
@@ -9,14 +11,24 @@ const {
   googleLogin,
 } = require("../controllers/authController");
 const { validateJwt, revalidateJwt } = require("../middlewares/processJwt");
+const { validateFields } = require("../middlewares/validateFields");
 
 router.get("/", getAllUsers);
 
 router.get("/user/:id", getUserById);
 
-router.post("/signup", signUpUser);
+router.post("/signup", [
+  check("name", "Name field is required").not().isEmpty(),
+  check("email", "Must be a valid email").isEmail(),
+  check("password", "Password must be 8 characters long with capital letter & Symbol")
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
+],signUpUser);
 
-router.post("/login", loginUser);
+router.post("/login", [
+  check("email", "You are required to enter the email").isEmail(),
+  check("password", "You are required to enter a password").not().isEmpty(),
+  validateFields
+] ,loginUser);
 
 router.post("/googleLogin", googleLogin)
 
